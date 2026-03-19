f16a43b47ff9b7032fcd90b48d501dec6db654f7
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2026-03-19 10:00:12 -0600

    Update menu paths for 0x0 Share functionality

--- python/0x0_share.py
+++ python/0x0_share.py
@@ -65,12 +65,14 @@ if not GTK_AVAILABLE:
         simpledialog = None
 
 MENU_PATHS = [
-    '$TAB/Share File via 0x0...',
-    '$CHAN/Share File via 0x0...',
-    '0x0 Share',
-    '0x0 Share/Share File...',
-    '0x0 Share/Preferences...',
-    '0x0 Share/Reset Host to Default',
+    '$TAB/0x0 Share',
+    '$TAB/0x0 Share/Share File...',
+    '$TAB/0x0 Share/Preferences...',
+    '$TAB/0x0 Share/Reset Host to Default',
+    '$CHAN/0x0 Share',
+    '$CHAN/0x0 Share/Share File...',
+    '$CHAN/0x0 Share/Preferences...',
+    '$CHAN/0x0 Share/Reset Host to Default',
 ]
 
 def _quote_for_menu(value):
@@ -501,13 +503,14 @@ def unload_cb(_userdata):
 
 
 def _register_menus():
-    _menu_add('$TAB/Share File via 0x0...', '0X0SHARE')
-    _menu_add('$CHAN/Share File via 0x0...', '0X0SHARE')
-
-    _menu_add('0x0 Share')
-    _menu_add('0x0 Share/Share File...', '0X0SHARE')
-    _menu_add('0x0 Share/Preferences...', '0X0SHARECONF')
-    _menu_add('0x0 Share/Reset Host to Default', '0X0SHARERESET')
+    _menu_add('$TAB/0x0 Share')
+    _menu_add('$TAB/0x0 Share/Share File...', '0X0SHARE')
+    _menu_add('$TAB/0x0 Share/Preferences...', '0X0SHARECONF')
+    _menu_add('$TAB/0x0 Share/Reset Host to Default', '0X0SHARERESET')
+    _menu_add('$CHAN/0x0 Share')
+    _menu_add('$CHAN/0x0 Share/Share File...', '0X0SHARE')
+    _menu_add('$CHAN/0x0 Share/Preferences...', '0X0SHARECONF')
+    _menu_add('$CHAN/0x0 Share/Reset Host to Default', '0X0SHARERESET')
 
 
 hexchat.hook_command(
